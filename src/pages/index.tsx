bbb6d0b9972b2b87bac0332424bd2ff071d01600
bruceji7 <revolverc24@gmail.com>
2021-10-12 10:39:23 +0900

    Add code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
+/*global chrome*/
+/* eslint-disable no-undef */
 import Head from "next/head";
-
-import Button from "../components/Button";
 
 import styles from "./index.module.scss";
 
+const SITE_SECRET = "Oxfords or Brogues?";
+
 export default function Home() {
+  const extID = "gmkcgfmaadbgpdfnkikppmoefannbmkf";
+
+  const sendtoExt = (msg: string) => {
+    console.log("attempt send message");
+
+    chrome.runtime.sendMessage(
+      extID,
+      { message: msg, secret: SITE_SECRET },
+      (response: any) => {
+        if (response.success) {
+          console.log(`WE GET SIGNAL:`, response);
+        } else {
+          console.log("OH NO: ", response);
+        }
+      }
+    );
+  };
+
   return (
     <div className={styles.Container}>
       <Head>
@@ -13,7 +33,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Welcome</h1>
-      <Button>Click Me</Button>
+      <button onClick={() => sendtoExt("EXTENSION, WAKE UP")}>Click May</button>
     </div>
   );
 }
